9edbd32615c6986199b6d57e3edf85f19879e771
Mark Shine <mark@ankra.io>
2023-07-31 08:40:01 +0200

    Added postgres

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,37 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpResponse, HttpServer, Responder, post};
+use diesel::prelude::*;
+use diesel::r2d2::{self, ConnectionManager};
 use dotenv::dotenv;
 use std::env;
+use serde::Deserialize;
 
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello, world!")
+use crate::models::{NewUser, User}; // assuming models.rs is in the same directory
+
+//...
+
+#[derive(Deserialize)]
+struct UserInfo {
+    name: String,
+    email: String,
+}
+
+#[post("/users")]
+async fn create_user(user_info: web::Json<UserInfo>, db_pool: web::Data<DbPool>) -> impl Responder {
+    let new_user = NewUser {
+        name: user_info.name.clone(),
+        email: user_info.email.clone(),
+    };
+    let conn = db_pool.get().expect("Failed to get DB connection");
+
+    HttpResponse::Ok().json(new_user) // Return the new user as JSON
 }
 
 fn config(cfg: &mut web::ServiceConfig) {
-    cfg.service(web::resource("/hello").route(web::get().to(hello)));
+    cfg.service(
+        web::scope("/api")
+            .service(hello)
+            .service(create_user)
+    );
 }
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    dotenv().ok();
-    env::var("RUST_LOG").unwrap_or_else(|_| env::set_var("RUST_LOG", "info"));
-    env_logger::init();
-
-    let server = env::var("SERVER").unwrap_or_else(|_| String::from("127.0.0.1:8080"));
-
-    HttpServer::new(|| {
-        App::new().configure(config)
-    })
-    .bind(server)?
-    .run()
-    .await
-}
-
+//...
